b81e8f7433d515b7bc6b81d56b0f4d9b062664db
KevynSilvaD <kevyn-silva-de-oliveira@hotmail.com>
2024-06-10 21:14:41 -0300

    mudando para o xaamp

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>modulo 2</title>
+    <link rel="style" href="css/bootstrap.min.css">
+   </head>
+<body>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <h1>ola mundo</h1>
+    <p>testando minha pagina</p>
+</body>
+</html>
