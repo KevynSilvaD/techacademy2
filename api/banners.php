5465c7bfd1d51e7ac8f364c7f17c503a7f1ea10a
KevynSilvaD <kevyn-silva-de-oliveira@hotmail.com>
2024-06-25 20:28:15 -0300

    mudanças finais

--- api/banners.php
+++ api/banners.php
@@ -4,9 +4,9 @@
 
     $path = "http://localhost/techacademy2/images/";
 
-    $banners[0] = array("nome"=>"Dream OF Gear","imagem"=>"{$path}banner1.png","id_game"=>1);
+    $banners[0] = array("nome"=>"Dream OF Gear","imagem"=>"{$path}banner1.png","id_game"=>3);
     $banners[1] = array("nome"=>"Street Fighter 6","imagem"=>"{$path}banner2.jpg","id_game"=>2);
-    $banners[2] = array("nome"=>"Mortal Kombal 1","imagem"=>"{$path}banner3.jpg","id_game"=>3);
+    $banners[2] = array("nome"=>"Mortal Kombal 1","imagem"=>"{$path}banner3.jpg","id_game"=>1);
    
 
     echo json_encode($banners);
--- pages/contato.php
+++ pages/contato.php
@@ -1,16 +1,17 @@
 <div class="container">
     <h1 class="text-center"><strong>Fale comigo!</strong></h1>
     <form name="formContato" method="post">
-        <label for="nome"><strong>Digite seu nome:</strong></label>
+        <label for="nome"><strong><H2>Digite seu nome:</strong></h2></label>
         <input type="text" name="nome" id="nome" required
         placeholder="nome completo"
         class="form-control form-control-lg">
-        <label for="email"><strong>Digite seu e-mail:</strong></label>
+        <label for="email"><strong><h2></h>Digite seu e-mail:</strong></h2></label>
         <input type="email" name="email" id="email"required
         placeholder="digite um email válido"
         class="form-control form-control-lg">
-    <butoon type="submit" class="btn btn-sucess btn-lg">
+
+        <butoon type="submit" class="margin btn btn-sucess btn-lg">
        <strong>ENVIAR MENSAGEM</strong>
-    </butoon>    
+    </butoon>
     </form>
 </div>
